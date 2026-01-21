23a2121d53b35d56805ae454ffe6d99d22a4189f
Anghielo Samaniego <samaniegoanghielo@gmail.com>
2026-01-20 20:59:11 -0500

    cambio de version

--- json_pe/__manifest__.py
+++ json_pe/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Consulta DNI y RUC',
-    'version': '17.0.1.0.0',
+    'version': '18.0.1.0.0',
     'category': 'Localization/Peru',
     'summary': 'Integración con API de json.pe para consultar DNI y RUC peruanos',
     'description': """
